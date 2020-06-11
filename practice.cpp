c3a4c5bffa52ba370c9af168be6a78b401989b08
JungMYEONG-jin <55305549+JungMYEONG-jin@users.noreply.github.com>
2020-06-11 13:58:33 +0900

    자리수 지정

---
+++ practice.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+
+
+    cin>>a>>b>>c>>d>>e;
+    cout<<a<<endl;
+    cout<<b<<endl;
+    cout<<c<<endl;
+    cout<<fixed;
+    cout.precision(3);
+    cout<<d<<endl;
+    cout<<fixed;
+    cout.precision(9);
+    cout<<e<<endl;
+
+
+    return 0;
+}
