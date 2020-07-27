b889892da825b5558a76fa425d7a4ccb72632a82
JMJ <dwrhksfl77@naver.com>
2020-07-27 23:53:01 +0900

    push_back 여러 원소 template

---
+++ push_back_template.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+
+template <typename T, typename... args>
+void push_back(T& t, args&&... arg)
+{
+	(t.push_back(arg), ...);
+	
+}
+
+
+int main()
+{
+	vector<int> my;
+
+	push_back(my, 12, 3, 4, 5, 6, 7, 8);
+
+	for (const auto& p : my)
+		cout << p << endl;
+
+}
