49be84b02a0b9a9169d06d74298b93bd4e55e48c
MJ1996 <55305549+JungMYEONG-jin@users.noreply.github.com>
2020-10-22 11:04:17 +0900

    Create is_sorted.cpp
    codewars solution

---
+++ is_sorted.cpp
@@ -0,0 +1,15 @@
+#include <algorithm>
+
+using namespace std;
+
+bool isAscOrder(std::vector<int> arr)
+{
+    // your code here
+    
+  return is_sorted(arr.begin(), arr.end());
+      
+  
+  
+  
+  
+}
