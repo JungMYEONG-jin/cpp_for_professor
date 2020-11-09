fc662f50e848cb79d7ba65cf0724a2485e2c1462
MJ1996 <55305549+JungMYEONG-jin@users.noreply.github.com>
2020-11-09 15:52:22 +0900

    Update Path_finder.cpp
    codewars 4kyu path_finder

--- Path_finder.cpp
+++ Path_finder.cpp
@@ -101,3 +101,101 @@ bool path_finder(string maze) {
 
 
 }
+
+
+// 이 풀이도 가능
+/*
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+bool isSafe(vector<vector<int>>& maze, int x, int y)
+{
+  if (x >= 0 && x < maze.size() && y >= 0 && y < maze.size() && maze[x][y] == 1)
+    return true;
+  return false;
+}
+
+
+
+
+bool maze_util(vector<vector<int>>& maze, int x, int y);
+
+bool path_finder(string maze) {
+  // TODO: Determine whether one can reach the exit at (n - 1, n - 1)
+  // starting from (0, 0) in a n × n maze (represented as a string)
+  // and return a boolean value accordingly
+
+  stringstream sstr(maze);
+  string res;
+  vector<vector<int>> temp;
+  vector<vector<char>> test;
+  
+  while (getline(sstr, res, '\n'))
+  {
+    vector<int> path2;
+    vector<char> test2;
+    for (auto i : res)
+    {
+      
+      if (i == '.')
+        path2.push_back(1);
+      else
+      {
+        path2.push_back(0);
+      }
+      
+      test2.push_back(i);
+    }
+    temp.push_back(path2);
+    test.push_back(test2);
+  }
+
+  vector<vector<int>> sol(temp.size(), vector<int>(temp.size(), 0));
+
+
+  if (maze_util(temp, 0, 0) == true)
+    return true;
+  return false;
+
+
+
+  
+}
+
+bool maze_util(vector<vector<int>>& maze, int x, int y)
+{
+  int N = maze.size();
+  if (x == N - 1 && y == N - 1 && maze[x][y] == 1)
+  {
+    maze[x][y] = 1;
+    return true;
+  }
+
+  //check maze is valid
+  if (isSafe(maze, x, y) == true)
+  {
+    maze[x][y] = 0;
+    // move forward
+    if (maze_util(maze, x - 1, y) == true)
+      return true;
+
+    // if forward failed then down move
+    if (maze_util(maze, x, y + 1) == true)
+      return true;
+    if (maze_util(maze, x +1, y) == true)
+      return true;
+    if (maze_util(maze, x, y - 1) == true)
+      return true;
+
+    // if both failed 
+    return false;
+  }
+  return false;
+
+}
+
+*/
