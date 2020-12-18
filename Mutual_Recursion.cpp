19d6daba4a96fc8a7859046874012bc340ec1ebe
MJ1996 <55305549+JungMYEONG-jin@users.noreply.github.com>
2020-12-18 22:54:56 +0900

    Create Mutual_Recursion.cpp
    codewars 6kyu

---
+++ Mutual_Recursion.cpp
@@ -0,0 +1,17 @@
+int F(int n);
+int M(int n);
+
+
+int F(int n) {
+  //...
+  if(n==0)
+    return 1;
+  return n-M(F(n-1));
+}
+
+int M(int n) {
+  //...
+  if(n==0)
+    return 0;
+  return n-F(M(n-1));
+}
