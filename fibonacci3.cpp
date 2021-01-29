e07c6ddce347bdf33f10a790958f424336394aa4
명진 <dwrhksfl77@naver.com>
2021-01-30 00:06:15 +0900

    매우큰수의 피보나치 구현하기

---
+++ fibonacci3.cpp
@@ -0,0 +1,99 @@
+#include <string>
+#include <algorithm>
+#include <regex>
+#include <iostream>
+#include <regex>
+using namespace std;
+
+bool isSmallers(string a, string b)
+{
+	int l1 = a.size();
+	int l2 = b.size();
+
+	if (l1 < l2 || l1 == l2 && a < b)
+		return true;
+	if (l1 > l2 || l1 == l2 && a > b)
+		return false;
+
+	if (a == b)
+		return true;
+	if (a > b)
+		return false;
+	return false;
+
+
+
+}
+
+
+std::string add(const std::string& a, const std::string& b) {
+
+		if (a.size() == 0 && b.size() == 0)
+			return "0";
+
+		if (a == "0" && b == "0")
+			return "0";
+
+		string t = a;
+		string k = b;
+		if (isSmallers(t, k))
+			swap(t, k);
+
+		int l1 = t.size();
+		int l2 = k.size();
+
+		k.insert(0, l1 - l2, '0');
+
+		string res;
+		int carry = 0;
+		for (int i = l1 - 1; i >= 0; i--)
+		{
+			int sum = t[i] - '0' + k[i] - '0' + carry;
+			if (sum >= 10)
+			{
+				carry = sum / 10;
+				sum = sum % 10;
+
+			}
+			else
+				carry = 0;
+			res.push_back(sum + '0');
+		}
+
+		res.push_back(carry + '0');
+
+
+
+		reverse(res.begin(), res.end());
+
+		res = regex_replace(res, regex("^0+"), "");
+
+		return res;
+	}
+
+
+int main()
+{
+
+
+int N;
+cin>>N;
+
+
+vector<string> res(10002);
+res[0]="0";
+res[1]="1";
+for(int i=2;i<=N;i++)
+{
+
+res[i] = add(res[i-1], res[i-2]);
+
+
+}
+
+cout<<res[N]<<endl;
+
+
+
+
+}
