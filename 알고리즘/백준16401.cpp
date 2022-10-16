bec11fb06164709f8eab76004c65ed8ab0b59dfb
MJ1996 <55305549+JungMYEONG-jin@users.noreply.github.com>
2022-10-16 11:36:49 +0900

    binary search

---
+++ 알고리즘/백준16401.cpp
@@ -0,0 +1,123 @@
+#include <iostream>
+#include <queue>
+#include <string>
+#include <cstring>
+#include <map>
+#include <string>
+#include <algorithm>
+#include <functional>
+#include <deque>
+#include <set>
+#include <numeric>
+#include <cmath>
+
+#define INF 1000000000
+
+using namespace std;
+
+int solution(vector<int> queue1, vector<int> queue2) {
+	int answer = -1;
+
+	int left = accumulate(queue1.begin(), queue1.end(), 0);
+	int right = accumulate(queue2.begin(), queue2.end(), 0);
+
+	queue<long, deque<long>> q1(deque<long>(queue1.begin(), queue1.end()));
+	queue<long, deque<long>> q2(deque<long>(queue2.begin(), queue2.end()));
+
+	int cnt = 0;
+	while (!q1.empty() && !q2.empty()) {
+		if (left > right) {
+			int val = q1.front();
+			q1.pop();
+			q2.push(val);
+			left -= val;
+			right += val;
+			cnt++;
+		}
+		else if (right > left) {
+			int val = q2.front();
+			q2.pop();
+			q1.push(val);
+			left += val;
+			right -= val;
+			cnt++;
+		}
+		else
+		{
+			break;
+		}
+
+	}
+
+	if (q1.empty() || q2.empty())
+		cnt = -1;
+	answer = cnt;
+	return answer;
+}
+
+string solution(vector<string> survey, vector<int> choices) {
+	string answer = "";
+	map<char, int> db;
+	for (int i = 0, len = survey.size(); i<len; i++) {
+		if (choices[i] == 4)
+			continue;
+		else if (choices[i] < 4) {
+			db[survey[i][0]] = db[survey[i][0]] + (4 - choices[i]);
+		}
+		else if (choices[i] > 4) {
+			db[survey[i][1]] = db[survey[i][1]] + (choices[i]-4);
+		}
+	}
+
+	string arr[] = { "RT", "CF", "JM", "AN" };
+	for (int i = 0; i < 4; i++) {
+		answer += (db[arr[i][0]] >= db[arr[i][1]] ? arr[i][0] : arr[i][1]);
+	}
+
+	return answer;
+}
+
+int main()
+{
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	int n, m, num;
+	cin >> n >> m;
+	vector<int> res;
+	for (int i = 0; i < m; i++) {
+		cin >> num;
+		res.push_back(num);
+	}
+
+	sort(res.begin(), res.end());
+
+	int start = 1;
+	int last = res.back();
+	int maxValue = 0;
+	while (start <= last) {
+		int mid = (start + last) >> 1;
+		// 6 
+		// mid 과자의 길이
+		int cnt = 0;
+		for (int i = 0; i < m; i++) {
+			cnt += res[i] / mid;
+		}
+
+		if (cnt >= n) {
+			start = mid + 1;
+			maxValue = max(maxValue, mid);
+		}
+		else {
+			last = mid - 1;
+		}
+	}
+	cout << maxValue << endl;
+
+
+
+    return 0;
+
+}
