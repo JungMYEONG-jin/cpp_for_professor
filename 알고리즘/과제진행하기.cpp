815d630c1740d009e14cef6d85d761cd1e162ba3
MJ1996 <55305549+JungMYEONG-jin@users.noreply.github.com>
2023-04-04 12:30:27 +0900

    조금 더 깔끔하게 푼 방법 추가

--- 알고리즘/과제진행하기.cpp
+++ 알고리즘/과제진행하기.cpp
@@ -49,3 +49,62 @@ vector<string> solution(vector<vector<string>> plans) {
     
     return answer;
 }
+
+```c++
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <stack>
+#include <map>
+
+using namespace std;
+
+vector<string> solution(vector<vector<string>> plans) {
+    vector<string> answer;
+    // sort by start time
+    sort(plans.begin(), plans.end(), [](auto it, auto it2){
+        return it[1]<it2[1];
+    });
+    // set time table
+    map<string, int> table;
+    
+    
+    // use stack
+    stack<string> db;
+    int time = 0;
+    for(const auto& it : plans){
+        int idx = it[1].find(':');
+        int hour = stoi(it[1].substr(0, idx));
+        int minute = stoi(it[1].substr(idx+1));
+        int nextTime = 60 * hour + minute;
+        while(!db.empty()){
+            string name = db.top();
+            db.pop();
+            int times = table[name];
+            table[name] -= nextTime - time;
+            
+            if(table[name]<=0){
+                answer.push_back(name);
+                time+=times;
+            }else{
+                db.push(name);
+                break;
+            }
+        }
+        time = nextTime;
+        table[it[0]] = stoi(it[2]);
+        db.push(it[0]);
+    }
+    
+    
+    while(!db.empty()){
+        answer.push_back(db.top());
+        db.pop();
+    }
+    
+    
+    return answer;
+}    
+    
+```
